8ccc79a85f4176f4cade4dcacc35a0e1ec7fbdff
Jens Finkhaeuser <jens@finkhaeuser.de>
2016-09-29 10:25:14 +0200

    Stop using external petstore.swagger.io service, but instead use a connexion based test server on localhost.

--- tests/test_swagger-tester.py
+++ tests/test_swagger-tester.py
@@ -5,6 +5,30 @@ import os
 
 from swagger_tester import swagger_test
 
+import connexion
+from multiprocessing import Process
+
+
+class ConnexionProcess(Process):
+    def run(self):
+        self.conn = connexion.App(
+            'tests',
+            debug=True,
+            specification_dir=os.path.dirname(__file__)
+        )
+        self.conn.add_api('swagger.yaml')
+        self.conn.app.run(port=8080)
+
+    def start(self):
+        Process.start(self)
+
+        import time
+        time.sleep(3)
+
+    def terminate(self):
+        Process.terminate(self)
+        Process.join(self)
+
 
 swagger_yaml_path = os.path.join(os.path.dirname(__file__), 'swagger.yaml')
 authorize_error = {
@@ -22,7 +46,7 @@ authorize_error = {
         '/v2/user/{username}': [200]
     }
 }
-swagger_io_url = 'http://petstore.swagger.io/v2'
+swagger_io_url = 'http://localhost:8080/v2'
 
 
 def test_swagger_test_use_example():
@@ -34,12 +58,18 @@ def test_swagger_test_dont_use_example():
 
 
 def test_swagger_test_specify_app_url_use_example():
+    conn = ConnexionProcess()
+    conn.start()
     swagger_test(app_url=swagger_io_url,
                  authorize_error=authorize_error,
                  use_example=True)
+    conn.terminate()
 
 
 def test_swagger_test_specify_app_url_dont_use_example():
+    conn = ConnexionProcess()
+    conn.start()
     swagger_test(app_url=swagger_io_url,
                  authorize_error=authorize_error,
                  use_example=False)
+    conn.terminate()
